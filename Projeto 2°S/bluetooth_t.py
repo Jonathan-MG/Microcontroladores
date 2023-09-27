940cb3217d62acffefb4385a5ac2b669f231eed2
Jonathan-MG <jonathanmgomes10@hotmail.com>
2023-09-27 07:50:26 -0300

    Fim do T2

--- Projeto 2°S/bluetooth_t.py
+++
@@ -1,9 +0,0 @@
-import serial
-import time
-
-bluetooth = serial.Serial("/dev/rfcomm0",9600)
-
-while True:
-    a=input("enter: ")
-    string='X{0}'.format(a)
-    bluetooth.write(string.encode('utf-8'))
