5a619e374dac2753a15af83450237324b7cdd140
Jonathan-MG <jonathanmgomes10@hotmail.com>
2023-10-05 16:13:36 -0300

    Códigos finais com todos os butões!

--- Projeto 2°S/Carro/Carro_Controller.py
+++ Projeto 2°S/Carro/Carro_Controller.py
@@ -38,6 +38,11 @@ def stop():
 def exit_app():
     #Código para sair do aplicativo
     sys.exit(0)
+    
+def center():
+    #Código para sair do aplicativo
+    string='C'
+    bluetooth.write(string.encode('utf-8'))
 
 # Crie uma janela
 window = tk.Tk()
@@ -70,13 +75,14 @@ btn_direction_forward = tk.Button(window, text="↑", bg=button_color, relief="s
 btn_direction_reverse = tk.Button(window, text="↓", bg=button_color, relief="solid", width=6, height=3, font=("Segoe UI Semibold", 14,"bold"), command=reverse, borderwidth=0, highlightthickness=0)
 btn_direction_left = tk.Button(window, text="←", bg=button_color, relief="solid", width=6, height=3, font=("Segoe UI Semibold", 14, "bold"), command=go_left, borderwidth=0, highlightthickness=0)
 btn_direction_right = tk.Button(window, text="→", bg=button_color, relief="solid", width=6, height=3, font=("Segoe UI Semibold", 14, "bold"), command=go_right, borderwidth=0, highlightthickness=0)
+btn_direction_center = tk.Button(window, text="O", bg=button_color, relief="solid", width=6, height=3, font=("Segoe UI Semibold", 14, "bold"), command=center, borderwidth=0, highlightthickness=0)
 
 # Organizando as setas
 btn_direction_forward.grid(row=6, column=0, padx=(0,15), pady=(150,10), sticky="e")
 btn_direction_reverse.grid(row=7, column=0, padx=(0,15), pady=10, sticky="e")
 btn_direction_left.grid(row=7, column=0, padx=(15,275), pady=10, sticky="w")  # Alinhado à direita
-btn_direction_right.grid(row=7, column=0, padx=(100,275), pady=10, sticky="w")  # Alinhado à esquerda
-
+btn_direction_right.grid(row=7, column=0, padx=(200,275), pady=10, sticky="w")  # Alinhado à esquerda
+btn_direction_center.grid(row=7, column=0, padx=(100,275), pady=10, sticky="w")
 # Posicionando o botão "PARE"
 btn_stop.grid(row=7, column=0, padx=(15,100), pady=10, sticky="e")
 
