b0e7cacd357d1d7cad9b41e714c0aaf783de1807
Jonathan-MG <jonathanmgomes10@hotmail.com>
2023-11-01 18:56:50 -0300

    Ubidots está conversando

--- Projeto 2°S/Carro/Carro_Controller.py
+++ Projeto 2°S/Carro/Carro_Controller.py
@@ -2,6 +2,41 @@ import tkinter as tk
 import serial
 import time
 import sys
+import requests
+
+
+TOKEN = "BBUS-J54N9Hq95Bfwin9E4Au5P9Xl9LtjAy"  # Put your TOKEN here
+DEVICE_LABEL = "nivel-agua"  # Put your device label here 
+VARIABLE_LABEL_1 = "nivel"  # Put your first variable label here
+
+def build_payload(variable_1,value):
+    value_1 = value
+    #print(value_1)
+    payload = {variable_1: value_1}
+    return payload
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+    # Processes results
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+    print("[INFO] request made properly, your device is updated")
+    return True
+
 
 bluetooth = serial.Serial("/dev/rfcomm0",9600)
 def inicio():
@@ -105,12 +140,16 @@ while True:
             valor = "".join(nivel)
             nivel = []
             medicao = int(valor)
+            payload = build_payload(VARIABLE_LABEL_1,medicao)
+            print("[INFO] Attemping to send data")
+            post_request(payload)
+            print("[INFO] finished")
             print(f'Medicao: {medicao}')
         else:
            nivel.append(char)
     #print(char)
     valor = ""
-    window.update()
+    
     
     
     
